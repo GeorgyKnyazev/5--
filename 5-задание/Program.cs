a8004e8023d10d8c3dad64c41144384191af3d96
Георгий Князев <saint_georgy@yahoo.com>
2022-05-28 10:49:57 +0300

    Добавьте файлы проекта.

---
+++ 5-задание/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _5_задание
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Имя";
+            string surName = "Фамилия";
+            Console.WriteLine(firstName + " " + surName);
+            (firstName, surName) = (surName, firstName);
+            Console.WriteLine(firstName + " " + surName);
+        }
+    }
+}
